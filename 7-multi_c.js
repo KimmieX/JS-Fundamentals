a8d7cbca0a24d13a6f8b752fc34b9f586fecf63a
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-05-25 22:41:40 +0000

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,3 +1,5 @@
+// Get the first argument and convert it to an integer
 const x = parseInt(process.argv[2]);
 
-console.log(Number.isNaN(x) || x < 1 ? "Missing number of occurrences" : Array(x).fill("C is fun").join("\n"));
+// Check if x is a valid number and print the appropriate message
+console.log(Number.isNaN(x) || x < 1 ? "Missing number of occurrences" : [...Array(x)].map(() => "C is fun").join("\n"));
