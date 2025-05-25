78b959c9618cedcd79173705ee43219582ee9dde
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-05-25 23:03:44 +0000

    5th update

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
 const x = process.argv[2];
 const count = parseInt(x);
 
-if (isNaN(count) || count <= 0) {
+if (isNaN(count)) {
     console.log("Missing number of occurrences");
-} else {
+} else if (count > 0) {
     let output = '';
     for (let i = 0; i < count; i++) {
         output += 'C is fun\n';
     }
     console.log(output.trim());
 }
+
